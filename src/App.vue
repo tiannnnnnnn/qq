5016741ae994b6a304097353e80bc568202ae3bf
許心恬 <hsuhsintien@xuxintiande-MacBook-Pro.local>
2021-01-24 19:00:56 +0800

    qq

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,47 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+     <!-- <div id="nav">
+         <router-link to="/">Home</router-link>   |
+         <router-link to="/about">About</router-link>   |
+         <router-link to="/week10">Week10</router-link>
+      </div> -->
+        <router-view/>
+
   </div>
+  
 </template>
 
+<script>
+
+export default {
+  
+}
+
+</script>
+
 <style>
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 60px;
 }
 
-#nav {
-  padding: 30px;
+.homee{
+  top: 4150px;
 }
+
+
 
 #nav a {
+  z-index: 1;
   font-weight: bold;
   color: #2c3e50;
+  
 }
-
 #nav a.router-link-exact-active {
   color: #42b983;
 }
